68a9fcaac88f268d3828b0ecbe016aba23439ca8
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-08-15 14:13:59 +0100

    Add temp dummy content to StateMachine

--- PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StateMachine.java
+++ PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StateMachine.java
@@ -1,18 +1,20 @@
 package com.gac2013.tdylf.pocketparamedic;
 
-/**
- * Created by demouser on 8/14/13.
- */
 public class StateMachine {
 
     public static final int NONE = -1;
     public static final int SCENE_SAFE = 0;
+    public static final int ID_A = 1;
+    public static final int ID_B = 2;
+    public static final int ID_C = 3;
 
-    public int current_state = SCENE_SAFE;
-    public State[] states = new State[]{};
 
-    public StateMachine(){
-    }
+    public int current_state = ID_A;
+    public State[] states = new State[]{
+            new State(ID_A, "A question", ID_B, ID_C, NONE, R.drawable.firstaid1),
+            new State(ID_B, "A question", ID_C, ID_A, NONE, R.drawable.helpme),
+            new State(ID_C, "A question", ID_A, ID_B, NONE, R.drawable.ic_launcher)
+    };
 
     public State getCurrentState(){
         return getStateById(current_state);
