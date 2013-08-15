b7b78eaebb62223653b7c37853e3d9d1b57d617f
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-08-15 17:46:13 +0100

    Change StartFragment item text color

--- PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StartFragment.java
+++ PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StartFragment.java
@@ -29,7 +29,7 @@ public class StartFragment extends Fragment {
                 "Heart attack"
         };
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, VALUES);
         listView.setAdapter(adapter);
 
