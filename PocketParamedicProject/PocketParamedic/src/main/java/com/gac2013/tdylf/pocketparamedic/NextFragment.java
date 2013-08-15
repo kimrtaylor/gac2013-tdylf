07a1cf972f5d1c8dcaa37203bfe55bf4b18c8f99
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-08-15 17:01:12 +0100

    Move csr.startRecognition() to a utterance-done callback

--- PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/NextFragment.java
+++ PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/NextFragment.java
@@ -3,9 +3,10 @@ package com.gac2013.tdylf.pocketparamedic;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +16,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class NextFragment extends Fragment
-        implements ContinuousSpeechRecognizer.RecognizedTextListener, TextToSpeech.OnInitListener {
+        implements ContinuousSpeechRecognizer.RecognizedTextListener,
+        TextToSpeech.OnInitListener {
 
     private ContinuousSpeechRecognizer csr;
     private Context context;
@@ -36,9 +39,9 @@ public class NextFragment extends Fragment
             Log.e(getClass().getName(), i + " -> " + fm.getBackStackEntryAt(i).getName());
 
         State currentState = StateMachine.getCurrentState();
-        ViewGroup vg = (ViewGroup)inflater.inflate(R.layout.instructions, container, false);
-        ((TextView)vg.findViewById(R.id.tvInstr)).setText("" + currentState.getId());
-        ((ImageView)vg.findViewById(R.id.ivInstr)).setImageResource(currentState.getImageResId());
+        ViewGroup vg = (ViewGroup) inflater.inflate(R.layout.instructions, container, false);
+        ((TextView) vg.findViewById(R.id.tvInstr)).setText("" + currentState.getId());
+        ((ImageView) vg.findViewById(R.id.ivInstr)).setImageResource(currentState.getImageResId());
 
         context = getActivity().getApplicationContext();
 
@@ -53,13 +56,13 @@ public class NextFragment extends Fragment
 
         csr = new ContinuousSpeechRecognizer(context);
         csr.setListener(this);
-        new Handler().postDelayed(new Runnable() {
+        /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 csr.startRecognition();
             }
         }, 3000);
-
+*/
     }
 
     @Override
@@ -86,19 +89,19 @@ public class NextFragment extends Fragment
     private void performYesTransition() {
         int state = StateMachine.getCurrentState().getYesAnswered();
         StateMachine.setCurrentState(state);
-        ((MainActivity)getActivity()).setupInstructionFragment();
+        ((MainActivity) getActivity()).setupInstructionFragment();
     }
 
     private void performNoTransition() {
         int state = StateMachine.getCurrentState().getNoAnswered();
         StateMachine.setCurrentState(state);
-        ((MainActivity)getActivity()).setupInstructionFragment();
+        ((MainActivity) getActivity()).setupInstructionFragment();
     }
 
     private void performDoneTransition() {
         int state = StateMachine.getCurrentState().getDoneAnswered();
         StateMachine.setCurrentState(state);
-        ((MainActivity)getActivity()).setupInstructionFragment();
+        ((MainActivity) getActivity()).setupInstructionFragment();
     }
 
     @Override
@@ -110,9 +113,35 @@ public class NextFragment extends Fragment
 
             if (result == TextToSpeech.LANG_MISSING_DATA
                     || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
+                Log.e("TTS", "This language is not supported");
+            } else {
+                tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                    @Override
+                    public void onStart(String utteranceId) {
+                    }
+
+                    @Override
+                    public void onDone(String utteranceId) {
+                        Log.e(getClass().getName(), "Utterance " + utteranceId + " completed");
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                csr.startRecognition();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(String utteranceId) {
+                    }
+                });
+
+                HashMap<String, String> myHashAlarm = new HashMap<String, String>();
+                myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_ALARM));
+                myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "SOME MESSAGE");
+
+                tts.speak(StateMachine.getCurrentState().getQuestion(), TextToSpeech.QUEUE_ADD, myHashAlarm);
             }
-            tts.speak(StateMachine.getCurrentState().getQuestion(), TextToSpeech.QUEUE_ADD, null);
         } else {
             Log.e("TTS", "Initialization Failed!");
         }
