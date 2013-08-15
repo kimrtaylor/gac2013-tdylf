a609e36a726d075fe4216a9e593816ad6aeb7e20
Frederik <frederik.gossen@gmail.com>
2013-08-15 15:37:04 +0100

    Add widget

--- PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StateMachine.java
+++ PocketParamedicProject/PocketParamedic/src/main/java/com/gac2013/tdylf/pocketparamedic/StateMachine.java
@@ -6,10 +6,18 @@ package com.gac2013.tdylf.pocketparamedic;
 public class StateMachine {
 
     public static final int NONE = -1;
-    public static final int SCENE_SAFE = 0;
+    public static final int ASK_SCENE_SAFE = 0;
+    public static final int DO_GLOVES = 1;
+    public static final int DO_SAVE_SCENE = 2;
+    public static final int ASK_CONSCIOUS = 3;
+    public static final int ASK_18 = 4;
 
-    public int current_state = SCENE_SAFE;
-    public State[] states = new State[]{};
+    public int current_state = ASK_SCENE_SAFE;
+    public State[] states = new State[]{ new State(ASK_SCENE_SAFE, "Is the scene safe? ", DO_GLOVES, DO_SAVE_SCENE, NONE, 0),
+                                         new State(DO_GLOVES, "Put on non-latex sterile gloves. ", NONE, NONE, ASK_CONSCIOUS, 0),
+                                         new State(DO_SAVE_SCENE, "Safe the scene. ", NONE, NONE, ASK_SCENE_SAFE, 0),
+                                         new State(ASK_CONSCIOUS, "Is the victim conscious? ", ASK_18, ASK_SCENE_SAFE, NONE, 0),
+                                         new State(ASK_18, "Is the victim over 18? ", NONE, NONE, ASK_SCENE_SAFE, 0)};
 
     public StateMachine(){
     }
